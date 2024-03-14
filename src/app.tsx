a7dd2c3342e387ee871d45917490af265a6fcd82
Gildemberg Leite <gildembergleite@gmail.com>
2024-03-14 09:09:30 -0300

    style: add bg color and styling text

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <>
-      <h1>Hello, World!</h1>
-    </>
+    <div className="flex w-screen h-screen justify-center items-center bg-zinc-900">
+      <h1 className="text-white font-bold text-4xl">Hello, World!</h1>
+    </div>
   )
 }
